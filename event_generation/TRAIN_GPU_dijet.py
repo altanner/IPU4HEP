0f6faa792d83c82d0fb2aa8a7446f7e964748340
altanner <8190834+altanner@users.noreply.github.com>
2020-09-18 00:45:04 +0100

    first refactor

--- event_generation/TRAIN_GPU_dijet.py
+++ event_generation/TRAIN_GPU_dijet.py
@@ -1,6 +1,10 @@
+import sys
+import time
 import numpy as np
 import argparse
+from tqdm import tqdm
 
+import tensorflow as tf
 from tensorflow.keras.layers import (
     Input,
     Flatten,
@@ -17,100 +21,116 @@ from tensorflow.keras.layers import (
     GRU,
     Lambda,
     Conv2DTranspose,
+    BatchNormalization,
+    LeakyReLU
 )
-from tensorflow.keras.layers import BatchNormalization
-from tensorflow.keras.layers import LeakyReLU
-from tensorflow.keras.models import Model, Sequential
-from tensorflow.compat.v1.keras.layers import UpSampling2D
-
-import tensorflow as tf
-
-# tf.disable_v2_behavior()
-
-import time
 from tensorflow.keras import backend as K
+from tensorflow.keras.models import Model, Sequential
+from tensorflow.compat.v1.keras.layers import UpSampling2D #! pylint hates this
+# tf.disable_v2_behavior()
+print(f"Using Tensorflow version {tf.__version__}")
+
 
 _EPSILON = K.epsilon()
+batch_size = 128 #? will this always be the same as the GAN_noise_size?
+GAN_noise_size = 128
+GAN_output_size = 7
+#! regression problem, not classification problem
+
+
+def assemble_training_dataset(batch_size):
+
+    #! train targets = normal dist mean = 0, variance = 1, 60000 samples, 7 dimensions
+    train_images = np.random.normal(0, 1, (60000, 7)) #!was 60000
+    train_images = train_images.reshape(train_images.shape[0], 7).astype("float32")
+    train_dataset = (
+        tf.data.Dataset.from_tensor_slices(train_images)
+        .batch(batch_size, drop_remainder=True)
+        .repeat(1)
+    )
+
+    return train_dataset
+
+
+def build_generator():
+
+    """
+    Build the network layers of the generator model.
+
+    Returns the model as "Generator".
+    """
+
+    G_input = Input(shape=(GAN_noise_size,))
+    G = Dense(128, kernel_initializer="glorot_uniform")(G_input)
+    G = LeakyReLU(alpha=0.2)(G)
+    G = BatchNormalization()(G)
+    G = Reshape([8, 8, 2])(G)  # default: channel last
+    G = Conv2DTranspose(32, kernel_size=2, strides=1, padding="same")(G)
+    G = LeakyReLU(alpha=0.2)(G)
+    G = BatchNormalization()(G)
+    G = Conv2DTranspose(16, kernel_size=3, strides=1, padding="same")(G)
+    G = LeakyReLU(alpha=0.2)(G)
+    G = BatchNormalization()(G)
+    G = Flatten()(G)
+    G_output = Dense(GAN_output_size)(G)
+    G_output = Activation("tanh")(G_output) #! hyperbolic tangent
+    Generator = Model(G_input, G_output)
+    Generator.summary()
+
+    return Generator
+
+
+def build_discriminator():
+
+    """
+    Build the network layers of the discriminator model.
+
+    Returns the model as "Discriminator".
+    """
+
+    D_input = Input(shape=(GAN_output_size,))
+    D = Dense(128)(D_input)
+    D = Reshape((8, 8, 2))(D)
+    D = Conv2D(64, kernel_size=3, strides=1, padding="same")(D)
+    D = LeakyReLU(alpha=0.2)(D)
+    D = Conv2D(32, kernel_size=3, strides=1, padding="same")(D)
+    D = LeakyReLU(alpha=0.2)(D)
+    D = Conv2D(16, kernel_size=3, strides=1, padding="same")(D)
+    D = LeakyReLU(alpha=0.2)(D)
+    D = Flatten()(D)
+    D = LeakyReLU(alpha=0.2)(D)
+    D = Dropout(0.2)(D)
+    D_output = Dense(1, activation="sigmoid")(D)
+    Discriminator = Model(D_input, D_output)
+    Discriminator.summary()
+
+    return Discriminator
+
+
+def assign_optimizers():
+
+    optimizer_stacked = tf.compat.v1.train.AdamOptimizer(
+        learning_rate=0.001,
+        beta1=0.9,
+        beta2=0.999,
+        epsilon=1e-08
+    )
+
+    optimizer_D = optimizer_stacked #! this was duplicated in OG
+
+    gen_optimizer = tf.keras.optimizers.Adam(0.0002)
+
+    disc_optimizer = gen_optimizer #! this was duplicated in OG
+
+    return optimizer_stacked, optimizer_D, gen_optimizer, disc_optimizer
 
 
 def _loss_generator(y_true, y_pred):
+
     y_pred = K.clip(y_pred, _EPSILON, 1.0 - _EPSILON)
-    out = -(K.log(y_pred))
+    out = -(K.log(y_pred)) #! pylint hates this (why does it still work?)
+
     return K.mean(out, axis=-1)
-
-
-print(tf.__version__)
-
-train_images = np.random.normal(0, 1, (60000, 7))
-
-print(np.shape(train_images))
-
-batch_size = 128
-
-train_images = train_images.reshape(train_images.shape[0], 7).astype("float32")
-
-
-train_dataset = (
-    tf.data.Dataset.from_tensor_slices(train_images)
-    .batch(batch_size, drop_remainder=True)
-    .repeat(1)
-)
-
-
-GAN_noise_size = 128
-GAN_output_size = 7
-
-# Build Generator model ...
-G_input = Input(shape=(GAN_noise_size,))
-G = Dense(128, kernel_initializer="glorot_uniform")(G_input)
-G = LeakyReLU(alpha=0.2)(G)
-G = BatchNormalization()(G)
-G = Reshape([8, 8, 2])(G)  # default: channel last
-G = Conv2DTranspose(32, kernel_size=2, strides=1, padding="same")(G)
-G = LeakyReLU(alpha=0.2)(G)
-G = BatchNormalization()(G)
-G = Conv2DTranspose(16, kernel_size=3, strides=1, padding="same")(G)
-G = LeakyReLU(alpha=0.2)(G)
-G = BatchNormalization()(G)
-G = Flatten()(G)
-G_output = Dense(GAN_output_size)(G)
-G_output = Activation("tanh")(G_output)
-Generator = Model(G_input, G_output)
-Generator.summary()
-
-
-# Build Discriminator model ...
-D_input = Input(shape=(GAN_output_size,))
-D = Dense(128)(D_input)
-D = Reshape((8, 8, 2))(D)
-D = Conv2D(64, kernel_size=3, strides=1, padding="same")(D)
-D = LeakyReLU(alpha=0.2)(D)
-D = Conv2D(32, kernel_size=3, strides=1, padding="same")(D)
-D = LeakyReLU(alpha=0.2)(D)
-D = Conv2D(16, kernel_size=3, strides=1, padding="same")(D)
-D = LeakyReLU(alpha=0.2)(D)
-D = Flatten()(D)
-D = LeakyReLU(alpha=0.2)(D)
-D = Dropout(0.2)(D)
-D_output = Dense(1, activation="sigmoid")(D)
-Discriminator = Model(D_input, D_output)
-Discriminator.summary()
-
-
-optimizer_D = tf.compat.v1.train.AdamOptimizer(
-    learning_rate=0.001, beta1=0.9, beta2=0.999, epsilon=1e-08
-)
-optimizer_stacked = tf.compat.v1.train.AdamOptimizer(
-    learning_rate=0.001, beta1=0.9, beta2=0.999, epsilon=1e-08
-)
-
-
-batch_size = 128
-
-
-gen_optimizer = tf.keras.optimizers.Adam(0.0002)
-
-disc_optimizer = tf.keras.optimizers.Adam(0.0002)
 
 
 @tf.function
@@ -122,8 +142,8 @@ def train_step(images):
         generated_images = Generator(noise)
 
         in_values = tf.concat([generated_images, images], 0)
-        labels_D_0 = tf.zeros((batch_size, 1))
-        labels_D_1 = tf.ones((batch_size, 1))
+        labels_D_0 = tf.zeros((batch_size, 1)) #! tf tensor of 0s, falses
+        labels_D_1 = tf.ones((batch_size, 1))  #! tf tensor of 1s, trues
 
         labels_D = tf.concat([labels_D_0, labels_D_1], 0)
 
@@ -148,35 +168,17 @@ def train_step(images):
     return loss_stacked, loss_D
 
 
-iteration = 0
+#!!! RUN THINGS
+train_dataset = assemble_training_dataset(batch_size)
+Generator = build_generator()
+Discriminator = build_discriminator()
+optimizer_stacked, optimizer_D, gen_optimizer, disc_optimizer = assign_optimizers()
+
 for epoch in range(10):
-    start = time.time()
-    total_gen_loss = 0
-    total_disc_loss = 0
 
-    for images in train_dataset:
-        iteration += 1
+    print(f"Epoch {epoch}")
 
-        if iteration == 2:
-            print("working")
-
-        if iteration == 200:
-            t0 = time.time()
-        if iteration == 1200:
-            t1 = time.time()
-
-            print("1000", t1 - t0)
-            t0 = time.time()
-        if iteration == 2200:
-            t1 = time.time()
-
-            print("2000", t1 - t0)
-            t0 = time.time()
-
-        if iteration == 3200:
-            t1 = time.time()
-
-            print("3000", t1 - t0)
-            t0 = time.time()
-            quit()
-        gen_loss, disc_loss = train_step(images)
+    with tqdm(total=None, file=sys.stdout, unit="its") as pbar:
+        for images in train_dataset:
+             gen_loss, disc_loss = train_step(images)
+             pbar.update(1)
